f804ca74c923ee01ef87906965e419726321f723
Raj Aryan Singh <rajlysm15@gmail.com>
2024-05-10 01:25:17 +0530

    styling for buttonf for solver page

--- KnapsackSolver.py
+++ KnapsackSolver.py
@@ -5,3 +5,35 @@ from PyQt5.QtCore import Qt
 class KnapsackSolver(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+        # Styling
+        self.setStyleSheet("font-size: 16px;")
+        # Layouts
+        back_layout = QVBoxLayout()
+        input_layout = QVBoxLayout()
+        main_layout = QVBoxLayout()
+        result_layout = QVBoxLayout()
+        buttons_layout = QHBoxLayout()
+        # Back Button
+        back_button = QPushButton("Back")
+        back_button.setFixedSize(330, 30)
+        back_button.setCursor(Qt.PointingHandCursor)  
+        back_button.setStyleSheet(
+             "QPushButton {"
+            "   font-size: 14px;"
+            "   border-radius: 10px;"
+            "   background-color: #C8CECF;"
+            "   color: #ffffff;"
+            "   font-style: italic;"
+            "}"
+            "QPushButton:hover {"
+            "   background-color: #BABFC0;"
+            "   font-size:15px;"
+            "}"
+        )
+        back_button.clicked.connect(self.gotoHome)
+        back_layout.addWidget(back_button)
+        back_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
