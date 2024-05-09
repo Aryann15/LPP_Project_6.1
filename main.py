b8fd10b9ed2d31c1efd06d466d89f2311cc7f2e3
Raj Aryan Singh <rajlysm15@gmail.com>
2024-05-10 01:29:17 +0530

    Initialized MainApp with window title and layout setup

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ from TransportationSolver import TransportationSolver
 class MainApp(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Operations Research Project")
+        self.setGeometry(330, 120, 1200, 800)
+        self.setStyleSheet("QMainWindow { background-color: #E1EFF3; }")
+        self.stack = QStackedWidget(self)
+        self.home_page = HomePage(self)
+        self.knapsack_solver = KnapsackSolver(self)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainApp()
+    ex.show()
+    sys.exit(app.exec_())
